c4a98863032d4d7b2c5547c8b35ea620118d2858
Patrick Heneise <patrick@zentered.co>
2022-06-23 17:10:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# GitEvents Discord Bot
+
+GitEvents is a set of automation tools and actions inspired by "Issue Ops". Events, talks, proposals etc. are powered by GitHub issues. This bot automatically creates the events on Discord.
+
+The bot is triggered by [GitEvents Propaganda](https://github.com/gitevents/propaganda).
+
+## Add the GitEvents Bot to your server
+
+[Add Bot](https://discord.com/api/oauth2/authorize?client_id=989117237619208233&scope=bot&permissions=8589953024)
+
+## Development
+
+For your convenience, this bot is already deployed on Cloudflare Workers. If you'd like to run your own worker, please follow the instructions below.
+
+- Install `wrangler` from Cloudflare: `npm i -g wrangler`
+- Create an application and bot on the [Discord Developer Portal](https://discord.com/developers)
+- Add the token, app id and public key to your Worker:
+  ```
+      wrangler secret put DISCORD_TOKEN
+      wrangler secret put DISCORD_PUBLIC_KEY
+      wrangler secret put DISCORD_APPLICATION_ID
+  ```
+- Run locally: `npm start`
+- Deploy the worker: `npm run deploy`
+
+## License
+
+Licensed under [MIT](./LICENSE).
---
+++ index.js
@@ -0,0 +1,70 @@
+import { decodeJwt, parseJwt } from '@cfworker/jwt'
+import bodyParser from '@zentered/issue-forms-body-parser'
+import { add } from 'date-fns'
+import pkg from 'date-fns-tz'
+const { zonedTimeToUtc } = pkg
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const oicd = request.headers.get('Authorization')
+  const decoded = decodeJwt(oicd)
+
+  const issuer = 'https://token.actions.githubusercontent.com'
+  const audience = decoded.payload.aud
+
+  const parsedJwt = await parseJwt(oicd, issuer, audience)
+
+  if (!parsedJwt.valid) {
+    console.error(parsedJwt.reason)
+    return
+  }
+
+  const data = await request.json()
+  const { serverId, timeZone, payload } = data
+  const parsedBody = await bodyParser(payload.issue.body)
+
+  // TODO: simplify date handling
+  const startTime = parsedBody.time
+  const startDate = parsedBody.date
+  const duration = parsedBody.duration?.duration
+  const content = parsedBody['event-description']
+  const location = parsedBody.location
+  const zonedDateTime = `${startDate.date}T${startTime.time}`
+
+  const utcDate = zonedTimeToUtc(zonedDateTime, timeZone)
+  const utcEndDate = add(utcDate, duration)
+
+  // TODO: adjust entity type and privacy level depending on online/offline events from issue body
+  const discordEventData = {
+    name: payload.issue.title,
+    scheduled_start_time: utcDate.toJSON(),
+    scheduled_end_time: utcEndDate.toJSON(),
+    privacy_level: 2,
+    entity_type: 3,
+    entity_metadata: {
+      location: location.text
+    }
+  }
+
+  const discordRequestOptions = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+      Authorization: `Bot ${DISCORD_TOKEN}`
+    },
+    body: JSON.stringify(discordEventData)
+  }
+
+  // console.log(JSON.stringify(discordEventData))
+
+  // TODO: response is always {}, should output errors
+  const apiResponse = await fetch(
+    `${DISCORD_API_URL}/guilds/${serverId}/scheduled-events`,
+    discordRequestOptions
+  )
+
+  return new Response('OK')
+}
